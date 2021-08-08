1d77e3a7673bffa8bfdf44933073e6a098c2babc
BuildTools <pruek.rodsawat@gmail.com>
2021-08-08 18:10:26 +0700

    echo bot

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
+console.clear()
+
 const Discord = require('discord.js')
 
-const client = new Discord.Client()
+const intents = new Discord.Intents(32767)
 
-client.once('ready', () => {
-    console.log('Hi')
+const client = new Discord.Client({ intents })
+
+client.on('ready', () => console.log('Bot is online'))
+
+client.on('messageCreate', (message) => {
+    if (!message.author.bot) message.reply(message.content)
 })
 
 client.login('ODI3ODQzNTkwMzg1MzAzNTUy.YGg7bA.gyue8Ap-Ycx51UmiG3h4hmkao7I')
