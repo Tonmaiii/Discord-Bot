b45ab83886880e53355314b304004cad9997e1c1
BuildTools <pruek.rodsawat@gmail.com>
2021-08-08 15:58:28 +0700

    bot online

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js')
+
+const client = new Discord.Client()
+
+client.once('ready', () => {
+    console.log('Hi')
+})
+
+client.login('ODI3ODQzNTkwMzg1MzAzNTUy.YGg7bA.gyue8Ap-Ycx51UmiG3h4hmkao7I')
