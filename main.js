06e3ec058c69ad9f30dcde0047e22466eab3bc20
BuildTools <pruek.rodsawat@gmail.com>
2021-09-27 12:29:19 +0700

    put commands in seperate files

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 console.clear()
 
-const Discord = require('discord.js')
+import { Client, Intents } from 'discord.js'
+import handle_command from './command_handler.js'
 
-const intents = new Discord.Intents(32767)
+const intents = new Intents(32767)
 
-const client = new Discord.Client({ intents })
+const client = new Client({ intents })
 
 client.on('ready', () => console.log('Bot is online'))
 
 client.on('messageCreate', (message) => {
-    if (!message.author.bot) message.reply(message.content)
+    if (message.content.startsWith('!')) {
+        let args = message.content.slice(1).split(' ')
+
+        try {
+            handle_command(args, message)
+        } catch (err) {
+            message.channel.send(`**\`Error:\`**\`\n${err}\``)
+        }
+    }
 })
 
 client.login('ODI3ODQzNTkwMzg1MzAzNTUy.YGg7bA.gyue8Ap-Ycx51UmiG3h4hmkao7I')
