b0051df570c15457fe548f4d5bc8c9c1643df67f
BuildTools <pruek.rodsawat@gmail.com>
2021-10-01 19:21:49 +0700

    remove ":" from vs

--- commands/ttt.js
+++ commands/ttt.js
@@ -25,7 +25,6 @@ module.exports = (message, args) => {
                             },
                             {
                                 name: 'VS',
-                                value: ':',
                                 inline: true
                             },
                             {
