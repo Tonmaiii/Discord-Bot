5e5e628e7c4270d6cf150e02c7b245648935630d
BuildTools <pruek.rodsawat@gmail.com>
2021-10-02 14:10:34 +0700

    change randomization code

--- commands/random.js
+++ commands/random.js
@@ -1,10 +1,10 @@
-module.exports = (message) => {
+module.exports = message => {
     let characters =
         'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890.,/\\;\'":<>?!@#$%^&*()_+'
 
     let result = ''
     for (let i = 0, length = Math.random() * 20; i < length; i++) {
-        result += characters[Math.floor(Math.random() * characters.length)]
+        result += characters[(Math.random() * characters.length) | 0]
     }
 
     message.channel.send(result)
