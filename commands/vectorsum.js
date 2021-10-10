7976fe5d857652588d11ecc8c8c7b27d94f5bb90
BuildTools <pruek.rodsawat@gmail.com>
2021-10-10 11:44:11 +0700

    vectorsum direction as degree

--- commands/vectorsum.js
+++ commands/vectorsum.js
@@ -1,7 +1,7 @@
 module.exports = (message, args) => {
     result = { x: 0, y: 0 }
 
-    args.splice(1).forEach((vector) => {
+    args.splice(1).forEach(vector => {
         values = vector.split(',')
         result.x += Math.cos((values[1] / 180) * Math.PI) * values[0]
         result.y += Math.sin((values[1] / 180) * Math.PI) * values[0]
@@ -10,6 +10,8 @@ module.exports = (message, args) => {
     let magnitude = Math.sqrt(result.x ** 2 + result.y ** 2)
     let direction = 1 / Math.cos(result.x / magnitude)
     message.channel.send(
-        `x: ${result.x}\ny: ${result.y}\n\nmag: ${magnitude}\ndir: ${direction}`
+        `x: ${result.x}\ny: ${result.y}\n\nmag: ${magnitude}\ndir: ${
+            (direction / (2 * Math.PI)) * 360
+        }`
     )
 }
