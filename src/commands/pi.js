44ff7d45ea970ac9dcdf158c2f70e2d0a168846c
BuildTools <pruek.rodsawat@gmail.com>
2021-11-05 13:47:19 +0700

    prevent empty message in 'empty' and 'pi'

--- src/commands/pi.js
+++ src/commands/pi.js
@@ -2,7 +2,7 @@ const pi = require('fs').readFileSync('assets/pi.txt', 'utf8')
 
 const handler = interaction => {
     const digits = interaction.options.get('digits')?.value
-    interaction.reply(pi.slice(0, digits <= 2000 ? digits : 10))
+    interaction.reply(pi.slice(0, 0 < digits <= 2000 ? digits : 10))
 }
 
 const info = {
