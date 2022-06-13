db9edfb93d01f830f864bd780299e493de356c1d
Tonmaiii <pruek.rodsawat@gmail.com>
2022-06-13 21:27:11 +0700

    better shape command with expiration

--- src/commands/shape.ts
+++ src/commands/shape.ts
@@ -6,12 +6,17 @@ const model = new TeachableMachine({
 })
 
 export const handler = async (interaction: CommandInteraction) => {
+    const startTime = Date.now()
     await interaction.reply({
-        content: 'Waiting for you to send an image',
+        content: `Waiting for you to send an image\n*This command will expire <t:${Math.round(
+            (startTime + 60 * 1000) / 1000
+        )}:R>*`,
         ephemeral: true
     })
 
     const callback = async (message: Message) => {
+        if (Date.now() - startTime > 60 * 1000)
+            interaction.client.removeListener('messageCreate', callback)
         if (message.channel.id !== interaction.channel.id) return
         if (message.author.id !== interaction.user.id) return
 
@@ -29,7 +34,9 @@ export const handler = async (interaction: CommandInteraction) => {
         } else {
             interaction
                 .followUp({
-                    content: 'You need to send an image',
+                    content: `You need to send an image\n*This command will expire <t:${Math.round(
+                        (startTime + 60 * 1000) / 1000
+                    )}:R>*`,
                     ephemeral: true
                 })
                 .catch(console.error)
