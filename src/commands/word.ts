aad4bffb49094f1429a60e05f87437f15cfc1b32
Tonmaiii <87060884+Tonmaiii@users.noreply.github.com>
2022-09-19 14:41:11 +0700

    Update word.ts

--- src/commands/word.ts
+++ src/commands/word.ts
@@ -3,8 +3,8 @@ import * as words from '../data/words.json'
 
 const handler = (interaction: CommandInteraction) => {
     const length = Math.floor(Math.random() * (10 - 3)) + 3
-    const word = words[length][Math.floor(Math.random() * words[length].length)
-    interaction.reply(result).catch(console.error)
+    const word = words[length][Math.floor(Math.random() * words[length].length)]
+    interaction.reply(word).catch(console.error)
 }
 
 const info = {
