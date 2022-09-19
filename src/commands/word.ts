459ba752055d325c7a6ce8d79ffcf8e89fffad57
Tonmaiii <87060884+Tonmaiii@users.noreply.github.com>
2022-09-19 14:36:27 +0700

    /word

---
+++ src/commands/word.ts
@@ -0,0 +1,15 @@
+import { CommandInteraction } from 'discord.js'
+import * as words from '../data/words.json
+
+const handler = (interaction: CommandInteraction) => {
+    const length = Math.floor(Math.random() * (10 - 3)) + 3
+    const word = words[length][Math.floor(Math.random() * words[length].length)
+    interaction.reply(result).catch(console.error)
+}
+
+const info = {
+    name: 'word',
+    description: 'Word'
+}
+
+module.exports = { handler, info }
